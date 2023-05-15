fa1454ceff3f89d37b77ab6c00325a7719fb8d42
latenitecoding <latenitecode@gmail.com>
2023-05-14 22:05:36 -0500

    adds cli and solutions mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+mod solutions;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    solution: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    solutions::call_solution(&args.solution);
 }
---
+++ src/solutions/mod.rs
@@ -0,0 +1,5 @@
+pub fn call_solution(solution: &str) -> () {
+    match solution {
+        _ => println!("No solution for {}", solution),
+    }
+}
