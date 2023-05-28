6a76c3c67881e6feedb556c279ed0825e22b4f3a
latenitecoding <latenitecode@gmail.com>
2023-05-27 22:44:25 -0500

    adds n2 restaurant opening solution

--- src/solutions/icpc/se_reg_22/restaurant_opening.rs
+++ src/solutions/icpc/se_reg_22/restaurant_opening.rs
@@ -40,3 +40,59 @@ pub fn main() {
     }
     println!("{}", min_cost);
 }
+
+pub fn main_n2() {
+    let (n, m) = {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        let params: Vec<usize> = buffer
+            .trim()
+            .split(' ')
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
+        (params[0], params[1])
+    };
+
+    let mut row_sums = [0u16; 50];
+    let mut col_sums = [0u16; 50];
+    let mut grid_sum = 0u32;
+    let mut cost_i = 0u32;
+    let mut buffer = String::new();
+    for i in 0..n {
+        io::stdin().read_line(&mut buffer).unwrap();
+        buffer
+            .trim()
+            .split(' ')
+            .map(|s| s.parse::<u8>().unwrap())
+            .enumerate()
+            .take(m)
+            .for_each(|(j, g_ij)| {
+                row_sums[i] += g_ij as u16;
+                col_sums[j] += g_ij as u16;
+                grid_sum += g_ij as u32;
+                cost_i += ((i as u32) + (j as u32)) * (g_ij as u32);
+            });
+        buffer.clear();
+    }
+
+    let mut min_cost = cost_i;
+
+    cost_i += grid_sum; // avoids needing to use conditional branching in the loops
+
+    let mut lhs_i = 0u32;
+    for i in 0..n {
+        cost_i = cost_i + 2 * lhs_i - grid_sum;
+
+        let mut cost_j = cost_i + grid_sum;
+        let mut lhs_j = 0u32;
+        for j in 0..m {
+            cost_j = cost_j + 2 * lhs_j - grid_sum;
+            min_cost = cmp::min(min_cost, cost_j);
+            lhs_j += col_sums[j] as u32;
+        }
+
+        lhs_i += row_sums[i] as u32;
+    }
+
+    println!("{}", min_cost);
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -11,6 +11,7 @@ pub fn call_solution(solution: &str) {
         "blueberry_waffle" => icpc::se_reg_22::blueberry_waffle::main(),
         "fading_wind" => icpc::se_reg_22::fading_wind::main(),
         "restaurant_opening" => icpc::se_reg_22::restaurant_opening::main(),
+        "restaurant_opening_n2" => icpc::se_reg_22::restaurant_opening::main_n2(),
         _ => println!("No solution for {}", solution),
     }
 }
