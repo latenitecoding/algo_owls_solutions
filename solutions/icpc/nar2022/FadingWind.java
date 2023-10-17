755f30505bee739aeda24b8e412dea29b91d8484
latenitecoding <latenitecode@gmail.com>
2023-10-17 18:48:00 -0500

    completes FadingWind

---
+++ solutions/icpc/nar2022/FadingWind.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class FadingWind {
+
+  private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+  // ========================================================
+  // StdIn Helpers
+  // ========================================================
+
+  @SuppressWarnings("unused")
+  private static int next() throws IOException {
+    return Integer.parseInt(reader.readLine());
+  }
+
+  @SuppressWarnings("unused")
+  private static int[] nextTuple() throws IOException {
+    return Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+  }
+
+  // ========================================================
+  // Solution
+  // ========================================================
+
+  public static void main(String[] args) throws IOException {
+    int[] hkvs = nextTuple();
+    int h = hkvs[0], k = hkvs[1], v = hkvs[2], s = hkvs[3], dist = 0;
+    while (h > 0) {
+      v += s;
+      v -= Math.max(1, v / 10);
+      if (v >= k) {
+        h++;
+      }
+      if (0 < v && v < k) {
+        h--;
+        if (h == 0) {
+          v = 0;
+        }
+      }
+      if (v <= 0) {
+        h = 0;
+        v = 0;
+      }
+      dist += v;
+      if (s > 0) {
+        s--;
+      }
+    }
+    System.out.println(dist);
+  }
+}
